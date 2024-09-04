3fa8f1a72fe284b98ec0dbc431d4bd9280c1009f
freedsondev <dev.freedson@gmail.com>
2024-09-04 11:09:37 -0300

    Ajustes no Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer bg-light text-center text-lg-start">
-      <div className="container p-4">
+      <div className="container">
         <p>© 2024 ONG. Todos os direitos reservados.</p>
       </div>
     </footer>
