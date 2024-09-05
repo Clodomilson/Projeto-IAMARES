9f5cfd6eb5dc4106c36a6c47b427c1fcf0c5d5e0
freedsondev <dev.freedson@gmail.com>
2024-09-05 17:46:18 -0300

    CNPJ adicionado no Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
     <footer className="footer bg-light text-center text-lg-start">
       <div className="container">
         <p>© 2024 ONG. Todos os direitos reservados.</p>
+        <p>CNPJ: 52.275.719/0001-39</p>
       </div>
     </footer>
   );
