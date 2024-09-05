a6eeecdd2128981eaf90c6f99e3a1b51f6cd0249
freedsondev <dev.freedson@gmail.com>
2024-09-05 14:07:05 -0300

    Layout.css alterado

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Home from './pages/Home';
 import Contatos from './pages/Contatos';
 // import Cadastro from './pages/Cadastro';
 import Sobre from './pages/Sobre';
 import Doacao from './pages/Doacao';
+import Layout from './components/Layout';
 
 const App = () => {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contatos" element={<Contatos />} />
-        {/* <Route path="/cadastro" element={<Cadastro />} /> */}
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/doacao" element={<Doacao />} />
-      </Routes>
-      <Footer />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contatos" element={<Contatos />} />
+          {/* <Route path="/cadastro" element={<Cadastro />} /> */}
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/doacao" element={<Doacao />} />
+        </Routes>
+      </Layout>
     </Router>
+    // <Router>
+    //   <Header />
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/contatos" element={<Contatos />} />
+    //     {/* <Route path="/cadastro" element={<Cadastro />} /> */}
+    //     <Route path="/sobre" element={<Sobre />} />
+    //     <Route path="/doacao" element={<Doacao />} />
+    //   </Routes>
+    //   <Footer />
+    // </Router>
+
   );
 };
 
