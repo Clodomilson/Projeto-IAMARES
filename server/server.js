d93576f43f94c06726e84e8ba3eced51239a8dca
Clodomilson <clodomilsonanjos.eng@outlook.com>
2024-09-19 11:15:46 -0300

    link de pagamento atualizado

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/api/getPaymentLink', (req, res) => {
   // Lógica para gerar ou obter o link de pagamento
-  const paymentLink = 'https://sualojacielo.com.br/superlink?id=123456';
+  const paymentLink = 'https://donate.stripe.com/28og2D8kq8557mM8wC';
   
   // Retorna o link de forma segura
   res.json({ link: paymentLink });
